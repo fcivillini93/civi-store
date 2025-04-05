213729cf13c25be3a4436257c9c521e7d6644ee2
fcivillini <fabio.civillini@objectway.it>
2025-04-05 12:12:48 +0200

    1.0.0: docs

--- civi-store-core/src/main/java/com/fcivillini/store_core/service/ProductService.java
+++ civi-store-core/src/main/java/com/fcivillini/store_core/service/ProductService.java
@@ -1,20 +1,52 @@
 package com.fcivillini.store_core.service;
 
-
 import com.fcivillini.store_core.model.Product;
 import com.fcivillini.store_interface.exc.StoreException;
 
 import java.util.List;
 
+/**
+ * Service interface for managing products.
+ */
 public interface ProductService {
+
+    /**
+     * Saves a new product.
+     *
+     * @param product the product to save
+     * @return the saved product
+     */
     Product save(Product product);
 
+    /**
+     * Updates an existing product.
+     *
+     * @param product the product to update
+     * @return the updated product
+     * @throws StoreException if the product cannot be updated
+     */
     Product update(Product product) throws StoreException;
 
+    /**
+     * Finds a product by its ID.
+     *
+     * @param id the ID of the product to find
+     * @return the found product
+     * @throws StoreException if the product cannot be found
+     */
     Product findById(Long id) throws StoreException;
 
+    /**
+     * Finds all products.
+     *
+     * @return a list of all products
+     */
     List<Product> findAll();
 
+    /**
+     * Deletes a product by its ID.
+     *
+     * @param id the ID of the product to delete
+     */
     void deleteById(Long id);
-
 }
--- civi-store-core/src/main/java/com/fcivillini/store_core/service/UserService.java
+++ civi-store-core/src/main/java/com/fcivillini/store_core/service/UserService.java
@@ -5,12 +5,39 @@ import com.fcivillini.store_interface.exc.StoreException;
 
 import java.util.List;
 
+/**
+ * Service interface for managing users.
+ */
 public interface UserService {
+
+    /**
+     * Finds a user by its ID.
+     *
+     * @param id the ID of the user to find
+     * @return the found user
+     * @throws StoreException if the user cannot be found
+     */
     User findById(Long id) throws StoreException;
 
+    /**
+     * Finds all users.
+     *
+     * @return a list of all users
+     */
     List<User> findAll();
 
+    /**
+     * Saves a new user.
+     *
+     * @param user the user to save
+     * @return the saved user
+     */
     User save(User user);
 
+    /**
+     * Deletes a user by its ID.
+     *
+     * @param id the ID of the user to delete
+     */
     void deleteById(Long id);
 }
